a1a2b17c466af6bc6465ad67ada2bf6aa7dbb95b
amansard <amelie.mansard@gmail.com>
2026-03-15 13:56:53 +0100

    Initial commit selenium test

---
+++ src/main/java/com/test/GoogleTest.java
@@ -0,0 +1,30 @@
+
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class GoogleTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        // On déclare le driver et on crée un ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // On ouvre Google
+        driver.get("https://www.google.fr");
+
+        // On maximise la fenêtre
+        driver.manage().window().maximize();
+
+        //on attends un max de dix secondes que les éléments chargent
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("L2AGLb"))).click();
+
+    }
+}
+
